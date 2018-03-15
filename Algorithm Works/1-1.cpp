cf73820c3a3549c5819314839fe28abcb8dcd26d
kagaya <kagaya85@foxmail.com>
2018-03-15 21:18:49 +0800

    统计数字问题

--- Algorithm Works/1-1.cpp
+++ Algorithm Works/1-1.cpp
@@ -6,30 +6,33 @@ using namespace std;
 int main()
 {
 	int n, num, temp, res[10] = { 0 }, high, low, div;
-	//ifstream fin;
+	
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
 
-	cin >> num;
-
+	//cin >> num;
+	fin >> num;
 	//统计位数
 	temp = num;
 	for (n = 0; temp; n++) {
-		temp %= 10;
+		temp /= 10;
 	}
 
 	//按位统计
 	for (int i = 0; i < n; i++) {
-		//统计0~9的个数
+		
 		div = 1;
 		for(int m = 0; m < i; m++){
 			div *= 10;
 		}
 
+		temp = num % (div * 10);
+		temp /= div;
+		high = num / (div * 10);
+		low = num % div;
+		//统计0~9的个数
 		for (int j = 0; j < 10; j++) {
 			//取出该位
-			temp = num % (div * 10);
-			temp /= div;
-			high = num / (div * 10);
-			low = num % div;
 			if(j > temp) {
 				res[j] += high * div;
 			}
@@ -37,13 +40,15 @@ int main()
 				res[j] += high * div + low + 1;
 			}
 			else {
-				res[j] += (high + 1) * div;
+				if(div == 1 || j != 0)
+					res[j] += (high + 1) * div;
 			}
 		}
 	}
 	
+	res[0] -= 1;	//自然数以1开头
 	for(int i = 0; i < 10; i++)
-		cout << res[i] << endl;
+		fout << res[i] << endl;
 
 	return 0;
 }
